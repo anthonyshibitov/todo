db311c379e5a6748f6ba24525bf17242b7f9cb6b
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-11-14 21:20:24 -0500

    Prep

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import Event from './eventClass.js';
 import List from './listClass.js';
+import Storage from './storageHandler.js';
 
 let eventTest = new Event('title','descrip','11/12/22', 'HIGH');
 let eventTest2 = new Event('title2', 'descrip2', '11/13/22', 'LOW');
@@ -9,24 +10,8 @@ let listTest = new List('list test');
 let listTest2 = new List('list test 2');
 listTest.addEvent(eventTest);
 
-console.log('event tets parent, shuold be 0', eventTest.getParent());
+const storage = new Storage();
+storage.writeList(listTest);
+const result = storage.getList();
 
-listTest.printMe();
-
-
-listTest2.addEvent(eventTest2);
-listTest2.printMe();
-
-console.log('event tets 2 parent, shuold be 1', eventTest2.getParent());
-
-console.log("stringify:", JSON.stringify(listTest2));
-console.log(listTest2);
-
-const testEventObject = {
-    "title": eventTest.getTitle(),
-    "description": eventTest.getDescription(),
-    "dueDate": eventTest.getDueDate(),
-    "priority": eventTest.getPriority()
-}
-
-console.log("string 2:", JSON.stringify(testEventObject));
+console.log(result);
